d7907156f0b9f8977a3ca5e50400b3bd54294e5a
Alejandro Orozco Builes <aorozcobuiles@gmail.com>
2019-04-11 17:41:46 -0500

    - Example project.

---
+++ ComponentsDesign/UI/Components/MyAppTextField.swift
@@ -0,0 +1,61 @@
+//
+//  MyAppTextField.swift
+//  ComponentsDesign
+//
+//  Created by Alejandro Orozco Builes on 3/27/19.
+//  Copyright © 2019 Alejandro Orozco Builes. All rights reserved.
+//
+
+import UIKit
+
+class MyAppTextField: DesignableView {
+    
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var textFieldName: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBInspectable
+    var name: String = String() {
+        didSet { configureTextField() }
+    }
+    
+    @IBInspectable
+    var text: String = String() {
+        didSet { configureTextField() }
+    }
+    
+    @IBInspectable
+    var placeholder: String = String() {
+        didSet { configureTextField() }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup(){
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+    
+    private func configureTextField(){
+        textFieldName.text = name
+        textField.text = text
+        textField.placeholder = placeholder
+    }
+
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+}
